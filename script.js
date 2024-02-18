d76d584346ea587db08b36bcec466e9d30f8adec
Peter Santyago <159805755+metaversecapitals@users.noreply.github.com>
2024-02-18 18:54:08 +0100

    Update #3

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-let resources = [];
-app.get('/resources', function(req, res) {
-    res.json(resources);
+app.get('/resources/:id/detail', function(req, res) {
+    let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
+    if (resourceIndex !== -1) {
+        res.json({
+            message: "Resource Details",
+            data: resources[resourceIndex]
+        });
+    } else {
+        res.status(404).json({
+            message: "No Resource Found with ID " + req.params.id,
+            data: {}
+        });
+    }
 });
-app.post('/resources', function(req, res) {
-    resources.push(req.body);
-    res.json(req.body);
-});
+
 app.get('/resources/:id', function(req, res) {
     let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
     if (resourceIndex !== -1) {
-        res.json(resources[resourceIndex]);
+        res.json({
+            message: "Resource Found",
+            data: resources[resourceIndex]
+        });
     } else {
-        res.sendStatus(404);
+        res.status(404).json({
+            message: "No Resource Found with ID " + req.params.id,
+            data: {}
+        });
     }
-});
-app.put('/resources/:id', function(req, res) {
-    let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
-    if (resourceIndex !== -1) {
-        resources[resourceIndex] = req.body;
-        res.json(resources[resourceIndex]);
-    } else {
-        res.sendStatus(404);
-    }
-});
-app.delete('/resources/:id', function(req, res) {
-    let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
-    if (resourceIndex !== -1) {
-        resources.splice(resourceIndex, 1);
-        res.send('Deleted Successfully');
-    } else {
-        res.sendStatus(404);
-    }
-});
-app.listen(3000, function() {
-    console.log('App is listening on port 3000!');
 });
