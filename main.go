3f6f550b1854458e22cb18ee65a613252b6a7a4e
Peter Santyago <159805755+metaversecapitals@users.noreply.github.com>
2024-02-18 18:51:46 +0100

    Update #5

--- main.go
+++ main.go
@@ -1,41 +1,22 @@
-package main
+package main 
 
 import (
-    "encoding/json"
-    "net/http"
-    "net/http/httptest"
-    "testing"
-
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
-func newRouter() *mux.Router {
-    r := mux.NewRouter()
-    r.HandleFunc("/items/count", getItemCount).Methods("GET")
-    return r
+var items []interface{}
+
+type ItemCountHandler struct{}
+
+func (h ItemCountHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(len(items))
 }
 
-func TestGetItemCount(t *testing.T) {
-    req, err := http.NewRequest("GET", "/items/count", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    rr := httptest.NewRecorder()
-    router := newRouter()
-    router.ServeHTTP(rr, req)
-
-    if status := rr.Code; status != http.StatusOK {
-        t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
-    }
-
-    var itemCount int
-    err = json.Unmarshal(rr.Body.Bytes(), &itemCount)
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    if itemCount != 0 {
-        t.Errorf("Handler returned unexpected body: got %v want %v", itemCount, 0)
-    }
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/items/count", ItemCountHandler{}).Methods(http.MethodGet)
+	http.ListenAndServe(":8000", r)
 }
