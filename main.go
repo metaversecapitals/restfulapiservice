4ae9a1d50db51cc2c2eaeb8f57721912dcf9fabc
Peter Santyago <159805755+metaversecapitals@users.noreply.github.com>
2024-02-18 18:57:25 +0100

    Update #7

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
-package main 
+type Item struct {
+	Name string `json:"name"`
+}
 
-import (
-	"encoding/json"
-	"net/http"
-	"github.com/gorilla/mux"
-)
-
-var items []interface{}
-
-type ItemCountHandler struct{}
-
-func (h ItemCountHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func getItems(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(len(items))
+	json.NewEncoder(w).Encode(items)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var item Item
+	_ = json.NewDecoder(r.Body).Decode(&item)
+	items = append(items, item)
+	json.NewEncoder(w).Encode(item)
 }
 
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/items", getItems).Methods("GET")
+	r.HandleFunc("/items", createItem).Methods("POST")
 	r.Handle("/items/count", ItemCountHandler{}).Methods(http.MethodGet)
 	http.ListenAndServe(":8000", r)
 }
