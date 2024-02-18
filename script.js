6136c39f5b10d2a533c07c7ac857750bde449e6f
Peter Santyago <159805755+metaversecapitals@users.noreply.github.com>
2024-02-18 18:48:25 +0100

    Update #2

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
-
 let resources = [];
-
 app.get('/resources', function(req, res) {
     res.json(resources);
 });
-
 app.post('/resources', function(req, res) {
     resources.push(req.body);
     res.json(req.body);
 });
-
 app.get('/resources/:id', function(req, res) {
     let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
     if (resourceIndex !== -1) {
@@ -24,7 +18,6 @@ app.get('/resources/:id', function(req, res) {
         res.sendStatus(404);
     }
 });
-
 app.put('/resources/:id', function(req, res) {
     let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
     if (resourceIndex !== -1) {
@@ -34,7 +27,6 @@ app.put('/resources/:id', function(req, res) {
         res.sendStatus(404);
     }
 });
-
 app.delete('/resources/:id', function(req, res) {
     let resourceIndex = resources.findIndex(resource => resource.id === parseInt(req.params.id));
     if (resourceIndex !== -1) {
@@ -44,7 +36,6 @@ app.delete('/resources/:id', function(req, res) {
         res.sendStatus(404);
     }
 });
-
 app.listen(3000, function() {
     console.log('App is listening on port 3000!');
 });
