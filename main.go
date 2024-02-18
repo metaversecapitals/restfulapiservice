41802587e34afb47ee50a41e174c13b4ee5a0dba
Peter Santyago <159805755+metaversecapitals@users.noreply.github.com>
2024-02-18 19:04:21 +0100

    Update #9

--- main.go
+++ main.go
@@ -2,23 +2,40 @@ type Item struct {
 	Name string `json:"name"`
 }
 
-func getItems(w http.ResponseWriter, r *http.Request) {
+func updateItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(items)
+	params := mux.Vars(r)
+	for idx, item := range items {
+		if item.Name == params["name"] {
+			items = append(items[:idx], items[idx+1:]...)
+			var newItem Item
+			_ = json.NewDecoder(r.Body).Decode(&newItem)
+			items = append(items, newItem)
+			json.NewEncoder(w).Encode(newItem)
+			return
+		}
+	}
+	http.Error(w, "Item not found", http.StatusNotFound)
 }
 
-func createItem(w http.ResponseWriter, r *http.Request) {
+func deleteItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var item Item
-	_ = json.NewDecoder(r.Body).Decode(&item)
-	items = append(items, item)
-	json.NewEncoder(w).Encode(item)
+	params := mux.Vars(r)
+	for idx, item := range items {
+		if item.Name == params["name"] {
+			items = append(items[:idx], items[idx+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+	http.Error(w, "Item not found", http.StatusNotFound)
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/items", getItems).Methods("GET")
 	r.HandleFunc("/items", createItem).Methods("POST")
-	r.Handle("/items/count", ItemCountHandler{}).Methods(http.MethodGet)
+	r.HandleFunc("/items/{name}", updateItem).Methods("PUT")
+	r.HandleFunc("/items/{name}", deleteItem).Methods("DELETE")
 	http.ListenAndServe(":8000", r)
 }
